a2fc88d19cfe82775be33bb7ecead027c5caa7d4
Roman Kudryavtsev <TTe4kiH98@gmail.com>
2021-05-02 15:02:33 +0300

    Manager added

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,13 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+    public Post[] search (int ownerId, String domain, String query, boolean ownersOnly, int count, int offSet) {
+        return null;
+    }
+    public void delete(int ownerId, int postId){
+    }
+
+}
